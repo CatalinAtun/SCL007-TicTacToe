b49ccd58ac8ea0592ebca9b51083d1ba6627690b
Dennisse Moreno C <catalina.camposcaceres9@gmail.com>
2019-04-29 16:21:38 -0400

    Cambio de switch por if

--- tictactoe/App.js
+++ tictactoe/App.js
@@ -3,10 +3,10 @@ import { StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native';
 import { MaterialCommunityIcons as Icon } from 'react-native-vector-icons'
 
 export default class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
-    this.state= {
+    this.state = {
       gameState: [
         [0, 0, 0],
         [0, 0, 0],
@@ -18,45 +18,53 @@ export default class App extends React.Component {
   }
 
   componentDidMount() {
-    this.startGame
+    this.startGame;
   }
 
-  startGame(){
-    this.setState({gameState:
-    [
-      [0, 0, 0],
-      [0, 0, 0],
-      [0, 0, 0]
-    ]})
+  startGame() {
+    this.setState({
+      gameState:
+        [
+          [0, 0, 0],
+          [0, 0, 0],
+          [0, 0, 0]
+        ]
+    })
   }
 
   gridTouch = (row, col) => {
     let value = this.state.gameState[row][col];
-    if (value !== 0) { 
-      return 
+    if (value !== 0) {
+      return
     }
-    
+
     let playerOne = this.state.playerOne;
 
     let arr = this.state.gameState;
     arr[row][col] = playerOne;
-    this.setState({gameState: arr});
+    this.setState({ 
+      gameState: arr 
+    });
 
-    let playerTwo = (playerOne == 1) ? -1 : 1;
-    this.setState({playerOne: playerTwo});
+    let playerTwo =
+      (playerOne == 1) ? -1 : 1;
+    this.setState({ 
+      playerOne: playerTwo 
+    });
   }
 
   showIcon = (row, col) => {
     let valueItems = this.state.gameState[row][col];
-    switch(valueItems)
-    {
-      case 1: return <Icon name="close" style={styles.xStyle}/>;
-      case -1: return <Icon name="circle-outline" style={styles.oStyle}/>;
-      default: return <View />;
+    if (valueItems === 1){
+      return <Icon name="close" style={styles.xStyle} />;
+    } if (valueItems === -1){
+      return <Icon name="circle-outline" style={styles.oStyle} />;
+    } else {
+      return <View />;
     }
   }
 
-  refresh(){
+  refresh() {
     this.setState({
       ...this.state,
       gameState: [
@@ -67,50 +75,50 @@ export default class App extends React.Component {
     })
   }
 
-
   render() {
     return (
       <View style={styles.container}>
+        <Text style={styles.title}>TIC TAC TOE</Text>
 
-      <Button
-  onPress={() => this.refresh()}
-  title="Refresh"
-  color="#841584"
-/>
+        <Button
+          onPress={() => this.refresh()}
+          title="Refresh"
+          color="#841584"
+        />
 
-        <View style={{flexDirection: 'row'}}>
+        <View style={{ flexDirection: 'row' }}>
           <TouchableOpacity onPress={() => this.gridTouch(0, 0)} style={[styles.borderView, { borderRightWidth: 1, borderBottomWidth: 1 }]}>
-          {this.showIcon(0, 0)}
+            {this.showIcon(0, 0)}
           </TouchableOpacity>
           <TouchableOpacity onPress={() => this.gridTouch(0, 1)} style={[styles.borderView, { borderRightWidth: 1, borderLeftWidth: 1, borderBottomWidth: 1 }]}>
-          {this.showIcon(0, 1)}
+            {this.showIcon(0, 1)}
           </TouchableOpacity>
           <TouchableOpacity onPress={() => this.gridTouch(0, 2)} style={[styles.borderView, { borderRightWidth: 2, borderLeftWidth: 1, borderBottomWidth: 1 }]}>
-          {this.showIcon(0, 2)}
+            {this.showIcon(0, 2)}
           </TouchableOpacity>
         </View>
 
-        <View style={{flexDirection: 'row'}}>
+        <View style={{ flexDirection: 'row' }}>
           <TouchableOpacity onPress={() => this.gridTouch(1, 0)} style={[styles.borderView, { borderRightWidth: 1, borderBottomWidth: 1, borderTopWidth: 1 }]}>
-          {this.showIcon(1, 0)}
+            {this.showIcon(1, 0)}
           </TouchableOpacity>
           <TouchableOpacity onPress={() => this.gridTouch(1, 1)} style={[styles.borderView, { borderRightWidth: 1, borderLeftWidth: 1, borderBottomWidth: 1, borderTopWidth: 1 }]}>
-          {this.showIcon(1, 1)}
+            {this.showIcon(1, 1)}
           </TouchableOpacity>
           <TouchableOpacity onPress={() => this.gridTouch(1, 2)} style={[styles.borderView, { borderLeftWidth: 1, borderBottomWidth: 1, borderTopWidth: 1 }]}>
-          {this.showIcon(1, 2)}
+            {this.showIcon(1, 2)}
           </TouchableOpacity>
         </View>
 
-        <View style={{flexDirection: 'row'}}>
+        <View style={{ flexDirection: 'row' }}>
           <TouchableOpacity onPress={() => this.gridTouch(2, 0)} style={[styles.borderView, { borderRightWidth: 1, borderTopWidth: 1 }]}>
-          {this.showIcon(2, 0)}
+            {this.showIcon(2, 0)}
           </TouchableOpacity>
           <TouchableOpacity onPress={() => this.gridTouch(2, 1)} style={[styles.borderView, { borderRightWidth: 1, borderLeftWidth: 1, borderTopWidth: 1 }]}>
-          {this.showIcon(2, 1)}
+            {this.showIcon(2, 1)}
           </TouchableOpacity>
           <TouchableOpacity onPress={() => this.gridTouch(2, 2)} style={[styles.borderView, { borderLeftWidth: 1, borderTopWidth: 1 }]}>
-          {this.showIcon(2, 2)}
+            {this.showIcon(2, 2)}
           </TouchableOpacity>
         </View>
 
@@ -120,15 +128,20 @@ export default class App extends React.Component {
 }
 
 const styles = StyleSheet.create({
+  title: {
+    fontSize: 40,
+    color: 'gray',
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
   borderView: {
     borderWidth: 2,
-    color: 'black',
+    borderColor: 'gray',
+    borderStyle: 'dotted',
     width: 100,
     height: 100,
     alignItems: 'center',
