37e8249e735a83758987a26c1a75263009fefe6a
Catalina Campos <catalina.camposcaceres9@gmail.com>
2019-04-28 23:21:38 -0400

    Iconos agregados

--- tictactoe/App.js
+++ tictactoe/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { MaterialCommunityIcons as Icon } from 'react-native-vector-icons'
 
 export default class App extends React.Component {
   render() {
@@ -7,21 +8,25 @@ export default class App extends React.Component {
       <View style={styles.container}>
 
         <View style={{flexDirection: 'row'}}>
-          <View style={styles.borderView}></View>
-          <View style={styles.borderView}><Text>Hola mundo</Text></View>
-          <View style={styles.borderView}></View>
+          <View style={[styles.borderView, { borderRightWidth: 1, borderBottomWidth: 1 }]}>
+          <Icon name="close" style={styles.xStyle}/></View>
+        
+          <View style={[styles.borderView, { borderRightWidth: 1, borderLeftWidth: 1, borderBottomWidth: 1 }]}>
+          <Icon name="circle-outline" style={styles.oStyle}/></View>
+          
+          <View style={[styles.borderView, { borderRightWidth: 2, borderLeftWidth: 1, borderBottomWidth: 1 }]}></View>
         </View>
 
         <View style={{flexDirection: 'row'}}>
-          <View style={styles.borderView}></View>
-          <View style={styles.borderView}></View>
-          <View style={styles.borderView}></View>
+          <View style={[styles.borderView, { borderRightWidth: 1, borderBottomWidth: 1, borderTopWidth: 1 }]}></View>
+          <View style={[styles.borderView, { borderRightWidth: 1, borderLeftWidth: 1, borderBottomWidth: 1, borderTopWidth: 1 }]}></View>
+          <View style={[styles.borderView, { borderLeftWidth: 1, borderBottomWidth: 1, borderTopWidth: 1 }]}></View>
         </View>
 
         <View style={{flexDirection: 'row'}}>
-          <View style={styles.borderView}></View>
-          <View style={styles.borderView}></View>
-          <View style={styles.borderView}></View>
+          <View style={[styles.borderView, { borderRightWidth: 1, borderTopWidth: 1 }]}></View>
+          <View style={[styles.borderView, { borderRightWidth: 1, borderLeftWidth: 1, borderTopWidth: 1 }]}></View>
+          <View style={[styles.borderView, { borderLeftWidth: 1, borderTopWidth: 1 }]}></View>
         </View>
 
       </View>
@@ -37,10 +42,24 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   borderView: {
-    borderWidth: 3,
+    borderWidth: 2,
     color: 'black',
     width: 100,
     height: 100,
     alignItems: 'center',
+  },
+  xStyle: {
+    color: "red",
+    fontSize: 60,
+    alignItems: "center",
+    justifyContent: "center",
+    flex: 1,
+  },
+  oStyle: {
+    color: "green",
+    fontSize: 60,
+    alignItems: "center",
+    justifyContent: "center",
+    flex: 1,
   }
 });
